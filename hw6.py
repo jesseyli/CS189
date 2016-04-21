fb4a9c0c07c088a6d6ce6244048d8b6f7639881a
Jesse <jesseyli@berkeley.edu>
2016-04-20 18:55:37 -0700

    Initial commit

---
+++ hw6.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+def trainNeuralNetwork(images,labels):
+	pass
+	# initialize all weights, V,W at random
+	# while (stopping criteria):
+	# 	pick one data point at random from training set
+	# 	perform forward pass (compute values for gradient descent update)
+	# 	perform backward pass 
+	# 	perform stochastic gradient update
+	# return V,W
+
